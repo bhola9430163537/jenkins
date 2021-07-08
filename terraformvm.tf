9c03e8c8dd06993a8ef0b58fe08667ab3e41a7b9
bhola9430163537 <87118082+bhola9430163537@users.noreply.github.com>
2021-07-08 14:09:23 +0530

    Update terraformvm.tf

--- terraformvm.tf
+++ terraformvm.tf
@@ -27,7 +27,7 @@ resource "azurerm_resource_group" "main" {
 
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
-  address_space       = ["11.0.0.0/16"]
+  address_space       = ["20.0.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
@@ -36,7 +36,7 @@ resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["11.0.2.0/24"]
+  address_prefixes     = ["20.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "main" {
